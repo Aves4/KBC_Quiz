9533fe50cb7fea8e8bfbd655d08c85ebafaa933c
Aves4 <muhammedaves22@gmail.com>
2024-10-08 19:45:48 +0530

    new changes

--- src/component/QRcodeComp.jsx
+++ src/component/QRcodeComp.jsx
@@ -5,14 +5,57 @@ const QRcodeComp = () => {
   const gamePage = "https://kbc-quiz-eight.vercel.app/mobilescreen";
   return (
     <div className="homescreen-container">
+      <div>
+        <h3>What is the purpose of React's useEffect hook?</h3>
+        <div style={{ padding: "20px" }}>
+          <div>
+            <input
+              type="radio"
+              id="value1"
+              value="To directly update the DOM"
+              name="answer"
+              className="each-option"
+            />
+            <label htmlFor="value1">To directly update the DOM</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="value2"
+              value="To manage side effects in a functional component"
+              name="answer"
+              className="each-option"
+            />
+            <label htmlFor="value2">
+              To manage side effects in a functional component
+            </label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="value3"
+              value="To define a component's state"
+              name="answer"
+              className="each-option"
+            />
+            <label htmlFor="value3">To define a component's state</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="value4"
+              value="To create reusable component logic"
+              name="answer"
+              className="each-option"
+            />
+            <label htmlFor="value4">To create reusable component logic</label>
+          </div>
+        </div>
+      </div>
       <div className="qr-container">
         <QRcode value={gamePage} size={300} />
       </div>
       <h2>Scan The QR Code to start Quiz on Mobile.</h2>
-      <h3>OR</h3>
-      <Link to="/mainscreen">
-        <button className="start-btn">Start Quiz on Desktop</button>
-      </Link>
     </div>
   );
 };
